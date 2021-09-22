ae337cf46679138d70fd6f7293125e4194e9343a
Sayan Goswami <goswami.sayan47@gmail.com>
2021-09-22 10:13:04 +0530

    Adds optimization, switches to tf backend.

--- examples/explore_sf.py
+++ examples/explore_sf.py
@@ -1,6 +1,8 @@
 import numpy as np
 import strawberryfields as sf
 
+NUM_SHOTS = 100
+
 def single_layer(params):
     # Creates a single mode quantum "program".
     # https://strawberryfields.readthedocs.io/en/stable/code/api/strawberryfields.Program.html
@@ -8,18 +10,18 @@ def single_layer(params):
 
     # Instantiate the Gaussian backend.
     # https://strawberryfields.readthedocs.io/en/stable/introduction/circuits.html
-    eng = sf.Engine("gaussian")
+    eng = sf.Engine("tf", backend_options={"cutoff_dim": 5})
 
     with prog.context as q:
         # Phase space squeezing gate.
         # https://strawberryfields.readthedocs.io/en/stable/code/api/strawberryfields.ops.Sgate.html
-        sf.ops.Sgate(params["displacement_magnitude"])  | q[0]
+        sf.ops.Dgate(params["displacement_magnitude"])  | q[0]
 
         # Measures whether a mode contains zero or nonzero photons.
         # https://strawberryfields.readthedocs.io/en/stable/code/api/strawberryfields.ops.MeasureThreshold.html
-        sf.ops.MeasureThreshold()                       | q[0]
+        sf.ops.MeasureFock()                       | q[0]
 
-    return eng.run(prog, shots=100)
+    return eng.run(prog, shots=NUM_SHOTS)
 
 
 if __name__ == "__main__":
