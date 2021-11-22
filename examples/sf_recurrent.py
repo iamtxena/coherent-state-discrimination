d87d6033bfd6359ac3f460bc54305f2d9e231353
Sayan Goswami <goswami.sayan47@gmail.com>
2021-11-22 16:48:05 +0530

    Updates structure.

--- examples/sf_recurrent.py
+++ examples/sf_recurrent.py
@@ -1,26 +1,29 @@
 import strawberryfields as sf
 from strawberryfields import ops
 import numpy as np
+from tensorflow import keras
 
 # Number of layers of the Dolinar receiver. Selecting 2 as the most basic,
 # non-trivial case.
-num_layers = 2
+num_layers = 4
 
 # Need k values for the splits of the coherent state. They need to sum one.
 # TODO: We need to decide whether this needs to be some specific value or should
 # I stick to the uniform split that I am doing presently.
 energy_splits = np.ones(num_layers)/num_layers
 
-def generate_nth_layer(layer_number, previous_measurement=None):
+def generate_nth_layer(layer_number, total_number_of_layers, predictor):
     """Generates the nth layer of the Dolinar receiver.
-    Given the `layer_number` and `previous_measurement` as input, it returns a
-    function that generates the necessary quantum circuit for the layer.
-    `previous_measurement` is a free parameter for the quantum circuit.
+    Given the `layer_number`, `total_number_of_layers` and `predictor` as input,
+    it returns a function that generates the necessary quantum circuit for the
+    n-th layer of the Dolinar receiver.
 
-    This function should also query the ML backend for the appropriate
-    displacement.
+    This function should query the ML backend for the appropriate displacement
+    magnitude using the predictor reference.
     """
+
     pass
+
 
 if __name__ == '__main__':
     # Basic 2-mode case.
