35059acea06840ffd31272ad3f09dbfb60a90bf5
Albert Solana <iamtxena@gmail.com>
2022-02-19 13:20:12 +0000

    feat: implemented top5 best codeboooks

---
+++ services/library/src/csd/best_codebook.py
@@ -0,0 +1,28 @@
+# best_codebook.py
+
+from dataclasses import dataclass
+from typing import List
+
+from csd.codeword import CodeWord
+
+
+@dataclass
+class BestCodeBook():
+    """Class for managing the best codebook."""
+    codebook: List[CodeWord]
+    measurement: List[List[int]]
+    success_probability: float
+    helstrom_probability: float
+    homodyne_probability: float
+
+    @property
+    def modes(self) -> int:
+        if len(self.codebook) <= 0:
+            return 0
+        return self.codebook[0].size
+
+    @property
+    def alpha(self) -> float:
+        if len(self.codebook) <= 0:
+            raise ValueError("empty codebook")
+        return self.codebook[0].alpha
